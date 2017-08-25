7e23ee25204deaef33449e40302ef2ed11de2061
Leonardo <leonardborj@gmail.com>
2017-08-25 15:24:45 -0600

    Operadores (), + y -

--- Matriz.h
+++ Matriz.h
@@ -35,7 +35,7 @@ class Matriz{
 		}
 
 		Matriz operator-(Matriz& value){
-			Matriz matrizRetorno(this->getSizeFila(), this->getSizeColumna());
+			Matriz matrizRetorno(this->getSizeFila(), value->getSizeColumna());
 			
 			//Dos matrices que se restan y matriz retorno
 			int** matrizr=matrizRetorno.getMatriz();
@@ -50,23 +50,45 @@ class Matriz{
 			matrizRetorno.setMatriz(matrizr);			
 			return matrizRetorno;
 		}
+
+		Matriz operator()(){
+			Matriz matrizRetorno(this->getSizeColumna, this->getSizeFila);
+			int** matrizr=matrizRetorno.getMatriz();
+			int** matriz1=this->getmatriz();
+
+			for(int i=0;i<this->getSizeColumna();i++){
+				for(int j=0;j<this->getSizeFila();j++){
+					matrizr[i][j]=matriz1[j][i];
+				}
+			}
+			matrizRetorno.setMatriz(matrizr);
+			return matrizRetorno;
+		}
+
+
 		
-		Matriz operator*(Matriz& value){
-			Matriz matrizRetorno(this->getSizeFila(), this->getSizeColumna());
+		/*Matriz operator*(Matriz& value){
+			Matriz matrizRetorno(this->getSizeFila(), this.getSizeColumna());
 			
 			//Dos matrices que se suman y matriz retorno
 			int** matrizr=matrizRetorno.getMatriz();
 			int** matriz1=this->getMatriz();
 			int** matriz2=value.getMatriz();
 
-			for(int i=0;i<this->getSizeFila();i++){
-				for(int j=0;j<this->getSizeColumna();j++){
-					matrizr[i][j]=matriz1[i][j]+matriz2[i][j];
+			for(int r=0;r<this->getSizeFila();r++){
+				for(int k=0;k<value.getSizeColumna();k++){
+					
+
+					for(int i=0;i<10;i++){
+						for(int j=0;j<10;j++){
+
+						}
+					}
 				}
 			}
 			matrizRetorno.setMatriz(matrizr);			
 			return matrizRetorno;
-		}
+		}*/
 		
 };
 #endif
