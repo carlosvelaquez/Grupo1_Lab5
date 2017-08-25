4dd862ddadff660fb4a1aa74652ffcbe71e7d505
Leonardo <leonardborj@gmail.com>
2017-08-25 14:08:45 -0600

    Carlos es gay

--- Matriz.h
+++ Matriz.h
@@ -16,6 +16,21 @@ class Matriz{
 		void setSizeColumna(int);
 		void setSizeFila(int);
 
+		Matriz operator+(Matriz& value){
+			Matriz matrizRetorno(this->getSizeFila(), this->getSizeColumna());
+			
+			//Dos matrices que se suman
+			int** matriz1=this->getMatriz();
+			int** matriz2=value.getMatriz();
+
+			for(int i=0;i<this->getSizeFila();i++){
+				for(int j=0;j<this->getSizeColumna();j++){
+					matrizRetorno[i][j]=matriz1[i][j]+matriz2[i][j];
+				}
+			}
+			return matrizRetorno;
+		}
+
 		
 };
 #endif
